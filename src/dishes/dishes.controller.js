1f931ddd42f953f43cca613f8544a23b1c7bcfad
dan <da.ga.mo.94@gmail.com>
2022-04-10 00:23:15 -0600

    complete dishes.controller - all tests passing for '/dishes' routes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,5 +1,6 @@
 const req = require("express/lib/request");
 const path = require("path");
+const { isNumber } = require("util");
 
 // Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
@@ -7,7 +8,6 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
 
 /* *********** MIDDLEWARE *********** *
  * ********************************** */
@@ -17,7 +17,7 @@ const bodyDataHas = (propertyName) => {
     return function (req, res, next) {
         const { data = {} } = req.body;
         res.locals.data = data;
-        data[propertyName] ? next() : next({ status: 400, message: `Dish must include a ${propertyName}` });
+        data[propertyName] ? next() : next({ status: 400, message: `Dish must include a ${propertyName === "price" ? propertyName + " greater than 0" : propertyName}` });
     }
 }
 
@@ -38,7 +38,28 @@ const dishExists = (req, res, next) => {
 
 }
 
-// VALIDATION FOR EACH PROPERTY AS OUTLINEd IN REQUIREMENTS
+// VALIDATION FOR EACH PROPERTY AS OUTLINED IN REQUIREMENTS
+const priceIsValid = (req, res, next) => {
+    const { data: { price } = {} } = req.body;
+    if (price <= 0 || !Number.isInteger(price)) {
+        return next({ status: 400, message: `price should be greater than 0.` })
+    }
+
+    return next();
+}
+
+// FOR 'PUT': MAKE SURE THE ID IS NOT BEING ALTERED YET STILL ALLOW TO UPDATE IF NO 'ID' PROPERTY IS PASSED IN/IS UNDEFINED
+const checkIdIsSame = (req, res, next) => {
+    const { data: { id } = {} } = req.body;
+    const { dishId } = req.params;
+    if (!id) { // IF NO ID IS FOUND, ALLOW TO UPDATE
+        return next();
+    } else if (dishId !== id) { // IF ID IS FOUND BUT DOES NOT MATCH ROUTE ID, SEND ERROR
+        return next({ status: 400, message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}` });
+    }
+    return next(); // UPDATE
+}
+
 
 
 /* ******** ROUTE HANDLERS ********** *
@@ -75,7 +96,7 @@ const update = (req, res) => {
     // alternative to code above by creating a copy in memory of the foundDish value instead of mutating the array by reference
     const temp = { ...dish, name, description, price, image_url }; // store a copy of the dish to be update in memory
     const dishToUpdateIndex = dishes.findIndex(dish => dish.id === temp.id); // find the index in the dishes array of the item to be updated
-    dishes[dishToUpdateIndex] = {...temp}; // overwrite object/item at the index found above with copy stored in memory (temp variable)
+    dishes[dishToUpdateIndex] = { ...temp }; // overwrite object/item at the index found above with copy stored in memory (temp variable)
     res.status(200).json({ data: temp });
 }
 
@@ -92,15 +113,18 @@ module.exports = {
         bodyDataHas("description"),
         bodyDataHas("price"),
         bodyDataHas("image_url"),
+        priceIsValid,
         create
     ],
     update: [ // add Middleware validation
+        dishExists,
         bodyDataHas("name"),
         bodyDataHas("description"),
         bodyDataHas("price"),
         bodyDataHas("image_url"),
-        dishExists,
+        priceIsValid,
+        checkIdIsSame,
         update
-    ], 
+    ],
     read: [dishExists, read]
 }
